ccf74637c1d85a84ed16959d67f357b4b1153275
AhmadRaid <52861533+AhmadRaid@users.noreply.github.com>
2023-03-07 17:29:30 +0200

    152

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const config = require("config");
+//const config = require("config");
 const app = express();
 // const port = config.get("server.port");
 // const host = config.get("server.host");
